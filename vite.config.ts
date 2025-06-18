7835ed3225f37f5d6b2c1f5b233e0a512e7cb0f4
kochizufan <kochizufan@gmail.com>
2025-06-18 23:44:09 +0900

    Update small error

--- vite.config.ts
+++ vite.config.ts
@@ -63,7 +63,6 @@ export default defineConfig({
       outDir: 'dist',
       exclude: ['tests'],
       rollupTypes: true,
-      skipDiagnostics: true,
       tsconfigPath: './tsconfig.json',
       logLevel: 'silent'
     })
